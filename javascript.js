60aa267cb86e9b3ed3dc2b2aeb954c63ae1b7b23
Lupis_ <doliver575@gmail.com>
2023-08-21 12:16:48 -0400

    Figure out how to append multiple divs"

--- javascript.js
+++ javascript.js
@@ -1,11 +1,22 @@
 
-const gridContainer = document.querySelector(".grid-container");
+const gridContainer = document.querySelector('.grid-container');
+let height = "500px";
+let width = "500px";
 
-const gridBlock = document.createElement("div");
-gridBlock.classList.add("grid");
 
-document.querySelector("#click").onclick = function(){
-    gridContainer.appendChild(gridBlock);
+
+document.querySelector('#click').onclick = function(){
+    gridContainer.innerHTML = '';
+    let size = prompt("How many cells?",2);
+    for (let i=0; i<size*size; i++){
+        const gridBlock = document.createElement('div');
+        gridBlock.classList.add('cell');
+        gridContainer.appendChild(gridBlock);
+    }
+}
+
+document.querySelector('#clear').onclick = function(){
+    gridContainer.innerHTML = '';
 }
 
 
