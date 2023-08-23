4ed17ee29afe8926e227cda7e4b1ce117113b846
Lupis_ <doliver575@gmail.com>
2023-08-23 10:34:50 -0400

    Complete Etch-a-Sketch Assignment All requirements of etch-a-sketch assignment completed RGB extra credit completed

--- javascript.js
+++ javascript.js
@@ -1,23 +1,59 @@
 
+const size = 0;
+let currentColor = 'black';
 const gridContainer = document.querySelector('.grid-container');
-let height = "500px";
-let width = "500px";
+
+const randomColor = () => {
+    let r = Math.floor(Math.random() * 256); // Random number from 0 - 255
+    let g = Math.floor(Math.random() * 256)
+    let b = Math.floor(Math.random() * 256);
+    return `rgb(${r},${g},${b})`;
+}
 
 
-
-document.querySelector('#click').onclick = function(){
+document.querySelector('#grid-size').onclick = function(){
     gridContainer.innerHTML = '';
-    let size = prompt("How many cells?",2);
+    let size = prompt("How big is the grid?",2);
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
     for (let i=0; i<size*size; i++){
-        const gridBlock = document.createElement('div');
-        gridBlock.classList.add('cell');
-        gridContainer.appendChild(gridBlock);
+        const gridCell = document.createElement('div');
+        gridCell.classList.add('grid-cell');
+        gridCell.addEventListener('mouseover', changeColor);
+        gridContainer.appendChild(gridCell);
+    }
+}
+document.querySelector('#solid-color').onclick = function(){
+    solidColor();
+}
+document.querySelector('#rainbow-color').onclick = function(){
+    rainbowColor();
+}
+document.querySelector('#erase').onclick = function(){
+    erase();
+}
+
+function changeColor(e) {
+    if (currentColor === 'black'){
+        e.target.style.backgroundColor = 'black';
+    }
+    else if (currentColor === 'rainbow'){
+        e.target.style.backgroundColor = randomColor();
+    }
+    else if (currentColor === 'erase'){
+        e.target.style.backgroundColor = 'transparent';
     }
 }
 
-document.querySelector('#clear').onclick = function(){
-    gridContainer.innerHTML = '';
+function solidColor() {
+    currentColor = 'black';
+    console.log(currentColor);
 }
-
-
+function rainbowColor() {
+    currentColor = 'rainbow';
+}
+function erase() {
+    currentColor = 'erase';
+}
 
